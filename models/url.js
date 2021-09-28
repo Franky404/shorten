5b2f727be7fe74660fc9869ece77e95f09f5bf1f
Frangky <81846311+Franky404@users.noreply.github.com>
2021-09-28 17:16:00 +0700

    Create url.js

---
+++ models/url.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var urlSchema = new Schema({
+  id:  String,
+  long_url: String,
+  count : {type: Number,default: 0},
+  created_at: Date
+});
+
+var Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;

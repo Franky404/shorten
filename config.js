558127fceddd96a3893935ad8073a665a396a632
Frangky <81846311+Franky404@users.noreply.github.com>
2021-10-16 15:52:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.db = {};
 // Your Domain
-config.webhost = 'http://s.frangky.me/';
+config.webhost = 'https://s.frangky.me/';
 // Database Mongodb
 // Example: mongodb+srv://Yourname:YourPassword@*********.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 config.db = 'mongodb+srv://Franky404:frangki12345@cluster0.u6x4y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';

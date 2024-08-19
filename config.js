034356346630cd7312341d63cfe93457cd2b4cc9
Frangky <81846311+Franky404@users.noreply.github.com>
2024-08-19 11:19:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.db = {};
 // Your Domain
-config.webhost = 'https://s.frangky.me/';
+config.webhost = 'https://web-production-f09f.up.railway.app/';
 // Database Mongodb
 // Example: mongodb+srv://Yourname:YourPassword@*********.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 config.db = 'mongodb+srv://anonymouschat404:Frangky404@cluster0.ijtxj.mongodb.net/?retryWrites=true&w=majority';

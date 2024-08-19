ff00a57ad018ca4c269bac8ba5a0a9c462a91739
Frangky <81846311+Franky404@users.noreply.github.com>
2024-08-19 11:06:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ config.db = {};
 config.webhost = 'https://s.frangky.me/';
 // Database Mongodb
 // Example: mongodb+srv://Yourname:YourPassword@*********.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-config.db = 'mongodb+srv://Franky404:frangki12345@cluster0.u6x4y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
+config.db = 'mongodb+srv://anonymouschat404:<password>@cluster0.ijtxj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+//mongodb+srv://anonymouschat404:<password>@cluster0.ijtxj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 module.exports = config;
